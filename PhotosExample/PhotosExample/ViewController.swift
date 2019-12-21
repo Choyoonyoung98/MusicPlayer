25b2f5f46633f1ae58c9235f09eb9299d3891b40
조윤영 <yoon980208@gmail.com>
2019-12-21 17:35:33 +0900

    [CREATE]사진첩 데이터 가져와서 변경하기

---
+++ PhotosExample/PhotosExample/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  PhotosExample
+//
+//  Created by 조윤영 on 21/12/2019.
+//  Copyright © 2019 조윤영. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, PHPhotoLibraryChangeObserver {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var fetchResult: PHFetchResult<PHAsset>!
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    let cellIdentifier: String = "cell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let photoAutorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAutorizationStatus {
+        case .authorized:
+            print("접근 허가됨")
+            self.requestCollection()
+            self.tableView.reloadData()
+        case .denied:
+            print("접근 불허")
+        case .notDetermined:
+            print("아직 응답하지 않음")
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                switch status {
+                case .authorized:
+                    print("사용자가 허용함")
+                    self.requestCollection()
+                    OperationQueue.main.addOperation {
+                        self.tableView.reloadData()
+                    }
+                    
+                case .denied:
+                    print("사용자가 불허함")
+                default: break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        @unknown default:
+            fatalError()
+        }
+        
+        //포토 라이브러리가 수정될 때마다 딜리게이트 메서드 호출
+        PHPhotoLibrary.shared().register(self)
+        
+        
+    }
+    
+    func requestCollection() {
+        
+        let cameralRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        
+        guard let cameraRollCollection = cameralRoll.firstObject else{
+            return
+        }
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+    }
+    
+    /*UITableViewDatasource*/
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchResult?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+        
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode:.aspectFill , options: nil, resultHandler: {image, _ in cell.imageView?.image = image
+        })
+        return cell
+    }
+    
+    
+    /*UITableViewDelegate*/
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let asset: PHAsset = self.fetchResult[indexPath.row]
+            
+            //change를 감지하고 삭제
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.deleteAssets([asset] as NSArray)
+            }, completionHandler: nil)
+        }
+    }
+
+    /*PHPhotoLibraryChangeObserver*/
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else {return}
+        
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
+    }
+    
+    
+
+}
+
